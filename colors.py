65df156185d88e1d7e3aaeda2c71afb8d0c19f53
kevmorlo <79797678+kevmorlo@users.noreply.github.com>
2023-02-24 15:28:03 +0100

    maj colors.py

--- colors.py
+++ colors.py
@@ -1,5 +1,25 @@
-BLEU = (0, 255, 255)
-ROUGE = (255, 0, 0)
-VERT = (0, 255, 0)
-VIOLET = (127, 0, 255)
-ETEINT = (0, 0, 0)
+class Color:
+
+    def __init__(self):
+        self.lst = [[0, 255, 255], [255, 0, 0], [0, 255, 0], [127, 0, 255], [0, 0, 0]]
+
+    def getList(self):
+        return self.lst
+
+    def check(self, r, g, b):
+
+        proche = None
+        for value in self.getList():
+            
+            if proche == None:
+                proche = value
+
+            else:
+
+                valeur_proche = [abs(proche[0] - r), abs(proche[1] - g), abs(proche[2] - b)]
+                valeur_essaie = [abs(value[0] - r), abs(value[1] - g), abs(value[2] - b)]
+
+                if valeur_proche > valeur_essaie:
+                    proche = value
+
+        return proche
