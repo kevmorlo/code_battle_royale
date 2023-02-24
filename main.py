ce673f2c4c974ff1492c52e4cbc07beb701e6f4a
Namania <81348116+Namania@users.noreply.github.com>
2023-02-24 15:39:51 +0100

    màj main.py

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from ova import *
+from colors import *
 import random
 import time
 import os
 
 robot:IRobot = OvaClientMqtt(id="ova1097bdcb48b1", arena="ishihara", username="epsirennes2023", server="192.168.10.103")
+color = Color()
 
 print("🟢 BEGIN TEST")
 robot.playMelody([("C4",50),("D4",50),("E4",50),("F4",50),("G4",50)])
@@ -13,6 +15,7 @@ robot.update()
 
 os.system('cls')
 print("📸 Test camera")
+
 while True:
 
 	sr = 0
@@ -20,14 +23,16 @@ while True:
 	sb = 0
 	n = 0
 
-	color = robot.getImagePixelRGB(robot.getImageWidth() // 2, robot.getImageHeight()-1)
-	sr = color[0]
-	sg = color[1]
-	sb = color[2]
+	getcolorbybot = robot.getImagePixelRGB(robot.getImageWidth() // 2, robot.getImageHeight()-1)
+	sr = getcolorbybot[0]
+	sg = getcolorbybot[1]
+	sb = getcolorbybot[2]
 	os.system('cls')
 	print("📸 Camera img "+str(robot.getImageWidth())+"x"+str(robot.getImageWidth())+" shot after "+str(robot.getImageTimestamp())+"ms")
 	print("🔴<R>="+str(sr)+" 🟢<G>="+str(sg)+" 🔵<B>="+str(sb))
-	robot.setLedColor(sr,sg,sb)
+	colors = color.check(sr, sg, sb)
+	print(f"{colors[0]}, {colors[2]}, {colors[1]}")
+	robot.setLedColor(colors[0], colors[2], colors[1])
 	robot.update()
 
 # End
